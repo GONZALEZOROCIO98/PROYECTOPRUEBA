11476e8d9aa15342c507a3331cedc0263b3cba1d
Gabriel González <160905@upslp.edu.mx>
2022-10-25 14:48:12 -0500

    BD request and showing the results

--- PHP/conection.php
+++ PHP/conection.php
@@ -3,13 +3,33 @@ $host="localhost";
 $user="root";
 $password="";
 $db="prueba";
-$con = mysqli_connect($host,$user,$password,$db);
-$query="SELECT * FROM `tarjetas` WHERE `usuario`=$_POST['idUsuario']";
-
-function ejecutar_consulta($query,$link)
+$link = mysqli_connect($host,$user,$password,$db);
+$int_value = intval( $_POST['idUsuario'] );
+$query="SELECT * FROM `tarjetas` WHERE `usuario`=$int_value";
+$registros=mysqli_query($link,$query) or die("Problemas en el select:".mysqli_error());
+if($registros)
     {
-        $registros=mysqli_query($link,$query) or die("Problemas en el select:".mysqli_error());
-        return $registros;
-    }
+        echo "MIS REGISTROS SON:<br>";
+        echo "<table border=0>";
+        echo "<tr bgcolor=#D8D8D8><td>FOLIO</td>";
+        echo "<td>BANCO</td>";
+        echo "<td>USUARIO</td>";
+        echo "<td>LIMITE DE CREDITO</td>"; 
+        echo "<td>SALDO TOTAL</td>";                                 
+        $i=1;
+
+        while($reg=mysqli_fetch_array($registros))
+        {
+            if($i%2==0) echo "<tr bgcolor=#99CCFF>";
+            else echo "<tr>";
+            echo "<td>".$reg['folio']."</td>";
+            echo "<td>",$reg['banco']."</td>";
+            echo "<td>",$reg['usuario']."</td>";
+            echo "<td>".$reg['limite']."</td>";
+            echo "<td>",$reg['saldo']."</td>";
+            $i++;
+        }
+        echo "</table>";
+    } 
 
 ?>
